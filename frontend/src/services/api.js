ad49d1a2bc9fe6e6e2baa4ccd965dfa3783699e6
vijethmd <vijethmd.cs23@bmsce.ac.in>
2026-06-10 11:21:58 +0530

    Update production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://autovalueai.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
